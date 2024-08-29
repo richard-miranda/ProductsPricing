dcb89b5aa6603636d3fde9469ce460a255b28372
Richard Miranda <richardmmianda@outlook.com>
2024-08-28 23:55:16 -0300

    Starting project

---
+++ ProductsPricing/Entities/ImportedProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsPricing.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+    }
+}
---
+++ ProductsPricing/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsPricing.Entities
+{
+    internal class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public String PriceTag()
+        {
+            return Name + "$" + Price;
+        }
+    }
+}
